009883917bb8bfeb5193cb5344cd85e2c16e6c9c
하린 김 <harin14@naver.com>
2020-05-29 23:24:04 +0900

    .

--- hanoi_harin.cpp
+++ hanoi_harin.cpp
@@ -8,8 +8,9 @@ SceneID scene1;
 ObjectID start;
 ObjectID initObjects[5];
 TimerID timerMixing;
-int mixCount = 31;
-const Second animationTime = 0.05f;
+int mixCount = 30;
+const Second animationTime = 0.5f;
+int i = 0;
 
 //각 'a' 'b' 'c' 열에 칸이 있는지 없는지 여부 파악용 
 int y[5] = { 190,240,290,340,390 };
@@ -43,8 +44,11 @@ int check1()
 	for (int i = 0; i < 5; i++)
 	{
 		if (y1[i] == 0)
-		y1[i] = 1;
-		return i;
+		{
+			y1[i] = 1;
+			return i;
+		} 
+		
 	}
 }
 int check2()
@@ -52,8 +56,10 @@ int check2()
 	for (int i = 0; i < 5; i++)
 	{
 		if (y2[i] == 0)
-		y2[i] = 1;
-		return i;
+		{
+			y2[i] = 1;
+			return i;
+		}
 	}
 }
 int check3()
@@ -61,8 +67,10 @@ int check3()
 	for (int i = 0; i < 5; i++)
 	{
 		if (y3[i] == 0)
-		y3[i] = 1;
-		return i;
+		{
+			y3[i] = 1;
+			return i;
+		}
 	}
 }
 
@@ -71,8 +79,10 @@ void check11()
 	for (int i = 4; i >= 0; i--)
 	{
 		if (y1[i] == 1)
-		y1[i] = 0;
-		return;
+		{
+			y1[i] = 0;
+			return;
+		}
 	}
 }
 void check22()
@@ -80,8 +90,10 @@ void check22()
 	for (int i = 4; i >= 0; i--)
 	{
 		if (y2[i] == 1)
-		y2[i] = 0;
-		return;
+		{
+			y2[i] = 0;
+			return;
+		}
 	}
 }
 void check33()
@@ -89,51 +101,72 @@ void check33()
 	for (int i = 4; i >= 0; i--)
 	{
 		if (y3[i] == 1)
-		y3[i] = 0;
-		return;
+		{
+			y3[i] = 0;
+			return;
+		}
 	}
 }
 
 //하노이 함수
-	void move( int n, int* a, int* b, int* c)
-{	
-		int i,k;
+void move(int n, int a, int b, int c)
+{
+	
 	if (n == 1) {
 		//printf("원판1 - %c to %c\n", a, b);
-		locateObject(initObjects[0], scene1, *b, 190);
+		if (b == 95)  i = check1();
+		if (b == 395) i = check2();
+		if (b == 695) i = check3();
+		if (a == 95)  check11();
+		if (a == 395) check22();
+		if (a == 695) check33();
+		locateObject(initObjects[0], scene1, b, y[i]);
+		setTimer(timerMixing, animationTime);
+		startTimer(timerMixing);
 	}
-	else {	
-	    move(n - 1, a, c, b);
-		if (*b == 95)  i = check1();
-		if (*b == 395) i = check2();
-		if (*b == 695) i = check3();
-		if (*a == 95)  check11();
-		if (*a == 395) check22();
-		if (*a == 695) check33();
-		locateObject(initObjects[n-1], scene1, *b, y[i]);
+	else {
+		move(n - 1, a, c, b);
+		if (b == 95)  i = check1();
+		if (b == 395) i = check2();
+		if (b == 695) i = check3();
+		if (a == 95)  check11();
+		if (a == 395) check22();
+		if (a == 695) check33();
+		locateObject(initObjects[n - 1], scene1, b, y[i]);
+		setTimer(timerMixing, animationTime);
+		startTimer(timerMixing);
 		//printf("원판%d - %c to %c\n", n, a, b);
 		move(n - 1, c, b, a);
+		setTimer(timerMixing, animationTime);
+		startTimer(timerMixing);
 	}
 	setTimer(timerMixing, animationTime);
 	startTimer(timerMixing);
 }
 
-int n=5, a = 95, b = 395, c = 695;
+int n = 5, a = 95, b = 395, c = 695;
 
 void mouseCallback(ObjectID object, int x, int y, MouseAction action)
 {
 	int a = 95, b = 395, c = 695;
 	if (object == start) {
 		timerMixing = createTimer();
-		move(n, &a, &b, &c);
+		setTimer(timerMixing, animationTime);
+		startTimer(timerMixing);
 	}
 }
 
 void timerCallback(TimerID timer)
 {
+
+	setTimer(timerMixing, animationTime);
+	startTimer(timerMixing);
 	if (mixCount > 0) {
-		--mixCount;	
-		move(n, &c, &b, &a);
+		--mixCount;
+	
+		move(n, c, b, a);
+		setTimer(timerMixing, animationTime);
+		startTimer(timerMixing);
 	}
 	setTimer(timerMixing, animationTime);
 	startTimer(timerMixing);
@@ -159,7 +192,7 @@ int main()
 	setTimerCallback(timerCallback);
 
 	scene1 = createScene("하노이탑", "main.png");
-	start = createObject("start.png", scene1, 100, 25, true);	
+	start = createObject("start.png", scene1, 100, 25, true);
 	put();
 
 	startGame(scene1);
